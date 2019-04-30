5ed7321e125e2faf2cb257d9bef2e81955162e7d
AcidFuel <boyarinoff.jr@yandex.ru>
2019-04-30 13:42:38 +0500

    Update b_Test.java

--- 1337/src/test/java/test/b_Test.java
+++ 1337/src/test/java/test/b_Test.java
@@ -2,6 +2,7 @@
 * Устанавливаем принадлежность класса к пакету testPackage
 */
 package test;
+
 /**
 * Импортируем библиотеку
 */
@@ -10,18 +11,22 @@ import static org.junit.Assert.*;
 * Импортируем библиотеку
 */
 import org.junit.*;
+
+import classPackage.classClientFrame;
 /**
 * Импортируем класс Calculator
 */
 import mainPackage.Calculator;
+
 /**
-* Основной класс
-*/
+ * Основной класс
+ */
 public class b_Test {
-/**
-* Объявляем переменную типа Calculator
-*/
-private Calculator test;
+	/**
+	 * Объявляем переменную типа Calculator
+	 */
+	private classClientFrame test;
+
 /**
 * Обозначаем , что следующий метод будет тестируемым
 */
@@ -29,14 +34,14 @@ private Calculator test;
 /**
 * тестируемый метод
 */
-public void testCalculator() {
+public void testCCF() {
 /**
 * Инициализируем переменную test
 */
-test = new Calculator();
+test = new classClientFrame();
 /**
 * Тестируем метод с входными параметрами 4, 6, 1.85, 1.75, 2450, 2250, 2050, 1.95 и с предполагаемым результатом 331256.25
 */
-assertEquals("331256.25", test.SetToutCost(4, 6, 1.85, 1.75, 2450, 2250, 2050, 1.95));
+assertNull(test.txtfield[1]);
 }
 }
